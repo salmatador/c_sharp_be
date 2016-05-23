fd3e2c42d34d9bcaa1977e77cdb175a9f8c66ba6
Micah Kline <salmatador@moonstub.com>
2016-05-23 16:01:10 -0700

    Still Buggy Input

--- WFA_Engine/Form1.Designer.cs
+++ WFA_Engine/Form1.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- WFA_Engine/Form1.cs
+++ WFA_Engine/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WFA_Engine.Framework;
 
 namespace WFA_Engine
 {
     public partial class Form1 : Form
     {
+        AbstractGame game;
+        
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            game = new AbstractGame(this);
+            game.start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(game.graphics.canvas, 0, 0);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            game.dispose();
         }
     }
 }
